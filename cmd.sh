ab14a081a38c8e2c359344c79801e49c80f413b5
Kindred-Genius <kindred-genius-0o@icloud.com>
2023-09-09 18:00:52 +0200

    test on selenium firefox

---
+++ cmd.sh
@@ -0,0 +1,8 @@
+
+sudo yum update
+sudo yum install git
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+
+conda create --name gethouses python=3.10
+conda activate gethouses
