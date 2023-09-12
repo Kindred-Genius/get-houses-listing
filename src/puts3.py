09a30831b11be2730e78211104e62bcb6a909e47
Kindred-Genius <kindred-genius-0o@icloud.com>
2023-09-12 17:37:50 +0200

    adding tmp cleaning up

--- src/puts3.py
+++ src/puts3.py
@@ -16,5 +16,9 @@ def put_csv_s3():
         for file in files:
             s3.meta.client.upload_file(f'{path}{file}', bucket_name, f'{year}/{month}/{day}/{file}')
 
+    # Deleting files from tmp
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+
 if __name__ == "__main__":
     put_csv_s3()
